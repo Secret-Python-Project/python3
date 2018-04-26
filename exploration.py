989474d798ebc8a5592673391be15d108044ee61
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-26 14:34:26 +0100

    Basic start for number wizard clone

---
+++ exploration.py
@@ -0,0 +1,13 @@
+import time
+
+print ("Welcome")
+time.sleep(1)
+
+min = 1
+max = 1000
+guess = 500
+
+print ("Think of a number between 1 and 1000")
+time.sleep(1)
+print ("I will try and guess it")
+
