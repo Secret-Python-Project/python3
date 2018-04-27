ab2f8ad4620ba231e6770f39084a837203908eb9
Michael Bridges <mike@michaelbridges.co.uk>
2018-04-27 15:14:13 +0100

    Review with Sam

--- exploration.py
+++ exploration.py
@@ -1,54 +1,56 @@
 def main():
-	minimum, maximum = introduce_game()
-	guess = get_inital_guess(minimum, maximum)
-	answer = start_guessing(guess)
-	processing_guess(answer, guess)
-	
+  minimum, maximum = introduce_game()
+  guess = get_guess(minimum, maximum)
+  answer = get_user_answer(guess)
+  processing_guess(answer, guess, minimum, maximum) #Order matters
+  #Scope Matteres
 
 def introduce_game():
-    #Set game conditions
-    minimum = 1
-    maximum = 1000
+  #Set game conditions
+  minimum = 1
+  maximum = 1000
 
-    print ("Welcome")
-    # Add a delay nof one second
-    print ("Think of a number between " + str(minimum) + " and " + str(maximum))
-    print ("I will try and guess it...\n")
-    return minimum , maximum
+  print ("Welcome")
+  # Add a delay nof one second
+  print ("Think of a number between " + str(minimum) + " and " + str(maximum))
+  print ("I will try and guess it...\n")
+ 
+  return minimum, maximum
 
-def get_inital_guess(minimum, maximum):
+def get_guess(minimum, maximum):
     
-    guess = (minimum + maximum )/ 2
-    return int(guess)
+  return int((minimum + maximum )/ 2)
 
-def start_guessing(guess):
-    print ("Is your number: " + str(guess))
-    answer = input("Please type yes, higher or lower \n")
-    return answer
+def get_user_answer(guess):
+  print("Is your number: " + str(guess) + "?")
+  answer = input("Please type yes, higher or lower \n")
+  return answer
 
-def processing_guess(answer, guess):
-    if answer == "yes":
-		game_won()
+def processing_guess(answer, guess, minimum, maximum):
+  if answer == "yes":
+    game_won()
 
-    elif answer == "higher":
-		print("OK, I'll guess a higher number...")
-		minimum = guess
-		start_guessing(guess)
+  elif answer == "higher":
+    print("OK, I'll guess a higher number...")
+    minimum = guess
+    new_guess = get_guess(minimum, maximum)
+    answer = get_user_answer(new_guess)
+    processing_guess(answer, new_guess, minimum, maximum)
 
-    elif answer == "lower":
-		print("OK, I'll guess a lower number...")
-		maximum = guess
-		start_guessing(guess)
+  elif answer == "lower":
+    print("OK, I'll guess a lower number...")
+    maximum = guess
+    new_guess = get_guess(minimum, maximum)
+    answer = get_user_answer(new_guess)
+    processing_guess(answer, new_guess, minimum, maximum)
 
-    else:
-		print("Please type a valid answer..")
-		start_guessing(guess)
-
-	return guess, minimum, maximum
+  else:
+    print("Please type a valid answer..")
+    answer = get_user_answer(guess)
+    processing_guess(answer, guess, minimum, maximum)
 
 def game_won():
-    print("Yes, I won! \n")
-    break
+  print("Yes, I won! \n")
 
 #Good luck explaining this one Mikey - "It's Magical" -Sam 2018 :D
 if __name__ == "__main__":
