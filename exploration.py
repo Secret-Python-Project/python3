6076af27e5577f0978e8bba02258cf54e7ade899
Ben Tristem <github@bentristem.com>
2018-05-01 21:57:25 +0100

    turned inside-out

--- exploration.py
+++ exploration.py
@@ -1,53 +1,57 @@
-def run_game():
-  minimum = 1
-  maximum = 1000
+def number_game():
 
-  introduce_game(minimum, maximum)
-  guess = make_guess(minimum, maximum)
-  answer = get_user_answer(guess)
-  process_guess(answer, guess, minimum, maximum) # Order matters
-  # Scope Matters
+  def introduce_game(minimum, maximum):
+    print ("Welcome")
+    # Add a delay of one second
+    print ("Think of a number between " + str(minimum) + " and " + str(maximum))
+    print ("I will try and guess it...\n")
+    return
 
-def introduce_game(minimum, maximum):
-  print ("Welcome")
-  # Add a delay of one second
-  print ("Think of a number between " + str(minimum) + " and " + str(maximum))
-  print ("I will try and guess it...\n")
-  return
+  def make_guess(minimum, maximum):
+    return int((minimum + maximum )/ 2)
+  
+  def game_won():
+    print("Yes, I won!\n")
 
-def make_guess(minimum, maximum):
-  return int((minimum + maximum )/ 2)
+  def process_guess(answer, guess, minimum, maximum): # reduce to 2 params max
+    if answer == "yes":
+      game_won()
 
-def get_user_answer(guess):
-  print("Is your number: " + str(guess) + "?")
-  answer = input("Please type yes, higher or lower \n")
-  return answer
+    elif answer == "higher":
+      print("OK, I'll guess a higher number...")
+      minimum = guess
+      new_guess = make_guess(minimum, maximum)
+      answer = get_user_answer(new_guess)
+      process_guess(answer, new_guess, minimum, maximum)
 
-def process_guess(answer, guess, minimum, maximum): # reduce to 2 params max
-  if answer == "yes":
-    game_won()
+    elif answer == "lower":
+      print("OK, I'll guess a lower number...")
+      maximum = guess
+      new_guess = make_guess(minimum, maximum)
+      answer = get_user_answer(new_guess)
+      process_guess(answer, new_guess, minimum, maximum)
 
-  elif answer == "higher":
-    print("OK, I'll guess a higher number...")
-    minimum = guess
-    new_guess = make_guess(minimum, maximum)
-    answer = get_user_answer(new_guess)
-    process_guess(answer, new_guess, minimum, maximum)
+    else:
+      print("Please type a valid answer..")
+      answer = get_user_answer(guess)
+      process_guess(answer, guess, minimum, maximum)
 
-  elif answer == "lower":
-    print("OK, I'll guess a lower number...")
-    maximum = guess
-    new_guess = make_guess(minimum, maximum)
-    answer = get_user_answer(new_guess)
-    process_guess(answer, new_guess, minimum, maximum)
+  def run_game():
+    minimum = 1
+    maximum = 1000
 
-  else:
-    print("Please type a valid answer..")
+    introduce_game(minimum, maximum)
+    guess = make_guess(minimum, maximum)
     answer = get_user_answer(guess)
-    process_guess(answer, guess, minimum, maximum)
+    process_guess(answer, guess, minimum, maximum) # Order matters
+    # Scope Matters
 
-def game_won():
-  print("Yes, I won!\n")
+  def get_user_answer(guess):
+    print("Is your number: " + str(guess) + "?")
+    answer = input("Please type yes, higher or lower \n")
+    return answer
+
+  run_game()
 
 # Calling from the terminal sets the calling module name as "__main__"
 # Because this code is at indentaion level 0 it is run first
@@ -56,4 +60,6 @@ def game_won():
 # run_game() must be defined before it can be called
 # ... so must all the other functions that run_game() calls
 if __name__ == "__main__":
-  run_game()
+  # game = NumberGame()
+  # game.run_game()
+  number_game()
